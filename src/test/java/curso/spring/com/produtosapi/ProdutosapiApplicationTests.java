782dd65282b13c18b04c5eaae7f050f20bf9759d
Walisson Santos <walissonsantost008@gmail.com>
2025-05-26 19:44:27 -0300

    Initial commit

---
+++ src/test/java/curso/spring/com/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package curso.spring.com.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
