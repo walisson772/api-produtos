89e0bc47e3d830c82ff70b7e090b011337931600
Walisson Santos <walissonsantost008@gmail.com>
2025-05-27 22:41:28 -0300

    Persistindo produtos no banco de dados

--- src/main/java/curso/spring/com/produtosapi/controller/ProdutoController.java
+++ src/main/java/curso/spring/com/produtosapi/controller/ProdutoController.java
@@ -1,18 +1,28 @@
 package curso.spring.com.produtosapi.controller;
 
 import curso.spring.com.produtosapi.model.Produto;
+import curso.spring.com.produtosapi.repository.ProdutoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    @Autowired
+    ProdutoRepository repository;
+
     @PostMapping
     public Produto salver(@RequestBody Produto produto){
         System.out.println("Produto recebido: " + produto);
+        var id = UUID.randomUUID().toString();
+        produto.setId(id);
+        repository.save(produto);
         return produto;
     }
 }
--- src/main/java/curso/spring/com/produtosapi/model/Produto.java
+++ src/main/java/curso/spring/com/produtosapi/model/Produto.java
@@ -1,10 +1,19 @@
 package curso.spring.com.produtosapi.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "produto")
 public class Produto {
 
+    @Id
+    @Column(name = "id")
     private String id;
+    @Column(name = "nome")
     private String nome;
+    @Column(name = "descricao")
     private String descricao;
+    @Column(name = "preco")
     private Double preco;
 
 
