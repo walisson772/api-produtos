242b472d73d7249b535a0c982cceb6927475ebd5
Walisson Santos <walissonsantost008@gmail.com>
2025-05-27 21:57:15 -0300

    Criando endpoint para recebimento dos produtos

---
+++ src/main/java/curso/spring/com/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,18 @@
+package curso.spring.com.produtosapi.controller;
+
+import curso.spring.com.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    @PostMapping
+    public Produto salver(@RequestBody Produto produto){
+        System.out.println("Produto recebido: " + produto);
+        return produto;
+    }
+}
---
+++ src/main/java/curso/spring/com/produtosapi/model/Produto.java
@@ -0,0 +1,52 @@
+package curso.spring.com.produtosapi.model;
+
+public class Produto {
+
+    private String id;
+    private String nome;
+    private String descricao;
+    private Double preco;
+
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "id='" + id + '\'' +
+                ", nome='" + nome + '\'' +
+                ", descricao='" + descricao + '\'' +
+                ", preco=" + preco +
+                '}';
+    }
+}
