d287baf8a352402d326ab8847b4f713e7f7ed908
Walisson Santos <walissonsantost008@gmail.com>
2025-05-30 20:37:32 -0300

    Metodos para deletar ,atualizar e buscar por parametro

--- src/main/java/curso/spring/com/produtosapi/repository/ProdutoRepository.java
+++ src/main/java/curso/spring/com/produtosapi/repository/ProdutoRepository.java
@@ -3,5 +3,9 @@ package curso.spring.com.produtosapi.repository;
 import curso.spring.com.produtosapi.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends JpaRepository<Produto, String> {
+
+    List<Produto> findByName(String nome);
 }
